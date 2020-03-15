289d48fd27e17ea7ceb9f9fcc1a3838c70df2437
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-03-15 18:16:57 +0100

    missing /usr/local/bin in PATH after brew upgrade

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,7 @@ alias j8="jhome  -v 1.8.0_202"
 alias j11="jhome  -v 11.0.2"
 #set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home
 set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home
+set -gx PATH /usr/local/bin $PATH 
 set -gx PATH $HOME/bin $PATH
 set -gx KUMA_REPORTS_ENABLED false
 set -gx PATH $HOME/kong/kuma/build/artifacts-darwin-amd64/kumactl/ $PATH
