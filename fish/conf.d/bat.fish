587dc5d4a452822a83cffbdc4f653b42af7b743a
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-10-04 20:24:52 +0200

    replicate packages with fisher

--- fish/conf.d/bat.fish
+++ fish/conf.d/bat.fish
@@ -1,2 +1,2 @@
-alias bat 'bat --theme="Monokai Extended Light" --style="numbers,changes,header"'
+alias bat 'bat --theme="Solarized (light)" --style="numbers,changes"'
 complete -c bat -a '(__fish_complete_path)'
--- fish/functions/forget.fish
+++ fish/functions/forget.fish
@@ -1,3 +1,4 @@
+# When IP of machine changes there is warning about ssh fingerprint and you cannot log into machine. Execute `forget ip` to reset this machine from known_hosts
 function forget
   for host in $argv
     sed -i.bak "/$host/d" ~/.ssh/known_hosts
