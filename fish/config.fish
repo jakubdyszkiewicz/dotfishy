9363d6bd4f5d76f105b8e303a909d378d3a9a22f
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2020-10-04 20:13:50 +0200

    install fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,19 @@
-#set -gx GOPATH /Users/jakub.dyszkiewicz/allegro/go
-#set -gx PATH /usr/local/opt/php@5.6/bin $PATH
-#set -gx PATH /usr/local/opt/php@5.6/sbin $PATH
-#set -gx PATH $GOPATH/bin $PATH
-#set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
-#set -gx GROOVY_HOME /usr/local/opt/groovy/libexec
-alias j8="jhome  -v 1.8.0_202"
-alias j11="jhome  -v 11.0.2"
-#set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home
+#set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home
 set -gx PATH /usr/local/bin $PATH 
 set -gx PATH $HOME/bin $PATH
 set -gx KUMA_REPORTS_ENABLED false
 set -gx PATH $HOME/kong/kuma/build/artifacts-darwin-amd64/kumactl/ $PATH
 set -gx PATH $HOME/kong/kuma/build/artifacts-darwin-amd64/kuma-dp/ $PATH
-set -gx PATH /usr/local/opt/go@1.12/bin $PATH
 set -gx GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
 alias c="bat"
+
+# Rewrite Fisher installation path to not mix my files with Fisher files
+set -g fisher_path $HOME/.config/fish/fisher
+
+set -p fish_function_path fish_function_path[1] $fisher_path/functions
+set -p fish_complete_path fish_complete_path[1] $fisher_path/completions
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2>/dev/null
+end
