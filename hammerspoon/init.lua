3e575d42b43f148aed790f12ad91d632b5a13202
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2022-04-02 15:28:37 +0200

    refresh hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,28 +6,21 @@ hs.window.animationDuration = 0.00
 -- Keys
 hyper = {'cmd', 'alt', 'ctrl', 'shift'}
 
--- Helper
-function bindToHyper(keyFun)
-	for key, fun in pairs(keyFun) do
-		hs.hotkey.bind(hyper, key, function()
-			fun()
-		end)
-	end
-end
+-- Empty hyper keys: W, Q (buggy), space
 
 -- Application mappings
 appMaps = {
 	f = 'Google Chrome',
 	c = 'Messenger',
 	v = 'Visual Studio Code',
-	a = 'HipChat',
+	a = 'Discord',
 	s = 'Slack',
 	e = 'Goland',
 	r = 'ForkLift',
 	d = 'iTerm',
 	b = 'Bear',
 	z = 'Spotify',
-	t = 'Todoist',
+	t = 'Things3',
   	x = 'Calendar'
 }
 
@@ -45,25 +38,11 @@ hs.hotkey.bind(hyper,'g',function()
 	hs.grid.show()
 end)
 
--- hs.hotkey.bind(hyper, 'b', function()
-	-- hs.grid.snap(hs.window.focusedWindow())
--- end)
-
--- Tabs
-
-hs.hotkey.bind(hyper, 'w', function()
-	hs.eventtap.keyStroke({"cmd", "shift"}, "]")
-end)
-
--- buggy, solve it!
-hs.hotkey.bind(hyper, 'q', function()
-	hs.eventtap.keyStroke({"cmd", "shift"}, "]")
-end)
-
 -- Resize
-hs.hotkey.bind(hyper, 'p', function() hs.grid.set(hs.window.focusedWindow(), '0,0, 4x2') end)
-hs.hotkey.bind(hyper, '[', function() hs.grid.set(hs.window.focusedWindow(), '4,0, 2x1') end)
-hs.hotkey.bind(hyper, ']', function() hs.grid.set(hs.window.focusedWindow(), '4,2, 2x1') end)
+hs.hotkey.bind(hyper, 'p', function() hs.grid.set(hs.window.focusedWindow(), '0,0, 3x1') end)
+hs.hotkey.bind(hyper, 'o', function() hs.grid.set(hs.window.focusedWindow(), '0,2, 3x1') end)
+hs.hotkey.bind(hyper, '[', function() hs.grid.set(hs.window.focusedWindow(), '3,0, 3x1') end)
+hs.hotkey.bind(hyper, ']', function() hs.grid.set(hs.window.focusedWindow(), '3,2, 3x1') end)
 hs.hotkey.bind(hyper, ';', function() hs.grid.set(hs.window.focusedWindow(), '0,0 3x2') end)
 hs.hotkey.bind(hyper, "'", function() hs.grid.set(hs.window.focusedWindow(), '3,0 3x2') end)
 hs.hotkey.bind(hyper, "\\", hs.grid.maximizeWindow)
@@ -77,34 +56,35 @@ end)
 
 -- Flow
 hs.hotkey.bind(hyper, 'return', function()
-	if hs.screen.mainScreen():name() == "Color LCD" then
-		adjustWindowsOfApp('iTerm2', '0,0 6x2')
-		adjustWindowsOfApp('Spotify', '0,0 6x2')
-		adjustWindowsOfApp('Google Chrome', '0,0 6x2')
-		adjustWindowsOfApp('Code', '0,0 6x2')
-		adjustWindowsOfApp('GoLand', '0,0 6x2')
-		adjustWindowsOfApp('Bear', '0,0 6x2')
-		adjustWindowsOfApp('Android Studio', '0,0 6x2')
-		adjustWindowsOfApp('Caprine', '4,1 2x1')
-		adjustWindowsOfApp('Slack', '3,1 3x1')
-		adjustWindowsOfApp('HipChat', '3,1 3x1')
-		adjustWindowsOfApp('Activity Monitor', '4,0 2x1')
-		adjustWindowsOfApp('Todoist', '4,0 2x1')
-	else
-		adjustWindowsOfApp('iTerm2', '0,0 4x2')
-		adjustWindowsOfApp('Spotify', '0,0 4x2')
-		adjustWindowsOfApp('Google Chrome', '0,0 4x2')
-		adjustWindowsOfApp('Code', '0,0 4x2')
-		adjustWindowsOfApp('GoLand', '0,0 4x2')
-		adjustWindowsOfApp('Bear', '0,0 4x2')
-		adjustWindowsOfApp('Android Studio', '0,0 4x2')
-		adjustWindowsOfApp('Caprine', '4,1 2x1')
-		adjustWindowsOfApp('Slack', '4,1 2x1')
-		adjustWindowsOfApp('HipChat', '4,1 2x1')
-		adjustWindowsOfApp('Activity Monitor', '4,0 2x1')
-		adjustWindowsOfApp('Todoist', '4,0 2x1')
+	if hs.screen.mainScreen():name() == "Built-in Retina Display" then
+		fullscreenAllWindows()
+	else -- 4K 32" screen
+		-- Left half
+		adjustWindowsOfApp('Google Chrome', '0,0 3x2')
+		adjustWindowsOfApp('Code', '0,0 3x2')
+		adjustWindowsOfApp('GoLand', '0,0 3x2')
+
+		-- Right half
+		adjustWindowsOfApp('iTerm2', '3,0 3x2')
+
+		-- Upper corner
+		adjustWindowsOfApp('Spotify', '3,0 3x1')
+		adjustWindowsOfApp('Bear', '3,0 3x1')
+		adjustWindowsOfApp('Things3', '4,0 2x1')
+		adjustWindowsOfApp('Calendar', '4,0 2x1')
+
+		-- Down corner
+		adjustWindowsOfApp('Messenger', '3,2 3x1')
+		adjustWindowsOfApp('Slack', '3,2 3x1')
+		adjustWindowsOfApp('Discord', '3,2 3x1')
 	end
 end)
+
+function fullscreenAllWindows()
+	for i, win in ipairs(hs.window:allWindows()) do
+		hs.grid.set(win, '0,0 6x4')
+	end
+end
 
 function adjustWindowsOfApp(appName, gridSettings)
 	local app = hs.application.get(appName)
@@ -117,14 +97,6 @@ function adjustWindowsOfApp(appName, gridSettings)
 			hs.grid.set(win, gridSettings)
 		end
 	end
-end
-
-function macbookScreen()
-	return hs.screen.find('Color LCD')
-end
-
-function dell27()
-	return hs.screen.find('DELL U2715H')
 end
 
 -- Mouse
@@ -144,11 +116,6 @@ hs.hotkey.bind(hyper, 'y', function()
 	hs.eventtap.leftClick(hs.mouse.getAbsolutePosition())
 end)
 
-hs.hotkey.bind(hyper, 'o', function()
-	hs.eventtap.rightClick(hs.mouse.getAbsolutePosition())
-end)
-
-
 -- Audio
 function switchAudio(name)
 	hs.alert('Switching to: ' .. name)
@@ -167,15 +134,7 @@ end)
 hs.hotkey.bind(hyper, '2', function()
 	hs.alert('WH-1000XM3')
 	-- hs.audiodevice.findDeviceByName('WH-1000XM3'):setDefaultOutputDevice()
-	-- there is some bug with Sonys to be selected. Fallback to applescript
-	done = hs.osascript.applescript([[
-tell application "System Events" to tell process "SystemUIServer"
-	tell menu bar item 7 of menu bar 1
-		click
-		click menu item "WH-1000XM3" of menu 1
-	end tell
-end tell
-	]])
+	-- there is some bug with Sonys to be selected :(
 end)
 
 hs.hotkey.bind(hyper, '3', function()
@@ -183,59 +142,12 @@ hs.hotkey.bind(hyper, '3', function()
 end)
 
 hs.hotkey.bind(hyper, '4', function()
-	switchAudio('(8D)Logitech Adapter')
-end)
-
-hs.hotkey.bind(hyper, '5', function()
 	switchAudio('Scarlett 2i2 USB')
 end)
 
 -- Spotify +/-
 hs.hotkey.bind(hyper, '-', hs.spotify.volumeDown)
 hs.hotkey.bind(hyper, '=', hs.spotify.volumeUp)
-
--- Clicking on notifications
-hs.hotkey.bind(hyper, '0', function()
-	done = hs.osascript.applescript([[
-my closeNotif()
-on closeNotif()
-	
-	tell application "System Events"
-		tell process "NotificationCenter"
-			set theWindows to every window
-			set this_item to item 1 of theWindows
-			try
-				click button 1 of this_item
-			on error
-				my closeNotif()
-			end try
-		end tell
-	end tell
-	
-end closeNotif
-	]])
-end)
-
-hs.hotkey.bind(hyper, '9', function()
-	done = hs.osascript.applescript([[
-my closeNotif()
-on closeNotif()
-	
-	tell application "System Events"
-		tell process "NotificationCenter"
-			set theWindows to every window
-			set this_item to item 1 of theWindows
-			try
-				click button 2 of this_item
-			on error
-				my closeNotif()
-			end try
-		end tell
-	end tell
-	
-end closeNotif
-	]])
-end)
 
 -- Reload config
 function reloadConfig(files)
@@ -278,20 +190,15 @@ function caffeinateCallback(eventType)
     end
 end
 
-hs.hotkey.bind(hyper, '6', function()
+hs.hotkey.bind(hyper, '9', function()
 	connectBluetooth(sonyBluetoothDeviceID)
 end)
 
-hs.hotkey.bind(hyper, '7', function()
+hs.hotkey.bind(hyper, '0', function()
 	disconnectBluetooth(sonyBluetoothDeviceID)
 end)
 
-hs.hotkey.bind(hyper, '8', function()
-	disconnectBluetooth(sonyBluetoothDeviceID)
-	hs.timer.usleep(1000)
-	connectBluetooth(sonyBluetoothDeviceID)
-end)
-
+-- Fix for MacOS behavior of messing up balance of bluetooth headphones
 hs.timer.doEvery(10, function()
     if (hs.audiodevice.current().device:balance() ~= 0.5) then
         hs.audiodevice.current().device:setBalance(0.5)
